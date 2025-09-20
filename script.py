0e00090b99e1383a5dea3c69ebe978d19fef6035
VedantMore2006 <vedantmoremain@gmail.com>
2025-09-20 17:47:05 +0530

    done

--- script.py
+++
@@ -1,6 +0,0 @@
-import requests
-url = "http://localhost:8000/detect/"
-files = {'file': open('s4.jpg', 'rb')}
-headers = {'x-api-key': 'gY5pR3L8aB1nS9eK4dH6cJ7mF2qV4oX'}
-response = requests.post(url, files=files, headers=headers)
-print(response.text)
